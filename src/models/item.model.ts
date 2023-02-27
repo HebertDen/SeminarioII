8532f1ec8d7b70369a7a13d485862dd0ea6aec2a
HebertDen <hebertdenilson1@gmail.com>
2023-02-27 00:42:45 -0500

    Subido

---
+++ src/models/item.model.ts
@@ -0,0 +1,40 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class Item extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: false,
+    required: true,
+  })
+  id: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  detalle: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  producto_id: number;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Item>) {
+    super(data);
+  }
+}
+
+export interface ItemRelations {
+  // describe navigational properties here
+}
+
+export type ItemWithRelations = Item & ItemRelations;
