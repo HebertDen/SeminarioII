219cc9db129718dbe07d6ab273a062259ffb8821
HebertDen <hebertdenilson1@gmail.com>
2023-05-09 13:43:38 -0500

    Subido

--- src/datasources/mysql.datasource.ts
+++ src/datasources/mysql.datasource.ts
@@ -8,8 +8,8 @@ const config = {
   host: '127.0.0.1',
   port: 3306,
   user: 'root',
-  password: '3lS3rv3r2021_HD%D4t4#Basic',
-  database: 'mydb'
+  password: '',
+  database: ''
 };
 
 // Observe application's life cycle to disconnect the datasource when
